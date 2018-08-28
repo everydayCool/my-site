d4aa1ddf2550cd48fc709afb3b593c3b7d3b1ce9
inner_peace <13338411482@163.com>
2018-08-28 10:38:09 +0800

    project test

--- my-site/src/main/java/com/huang/mysite/controller/UserController.java
+++ my-site/src/main/java/com/huang/mysite/controller/UserController.java
@@ -1,13 +1,55 @@
 package com.huang.mysite.controller;
 
+import com.huang.mysite.utils.APIResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Api("用户管理类")
 @Controller
+@RequestMapping(value = "/user")
 public class UserController {
 
 
+    @ApiOperation("登录")
+    @PostMapping(value = "/login")
+    @ResponseBody
+    public APIResponse tologin(HttpServletRequest request,
+                               HttpServletResponse response,
+                               @ApiParam(name = "username", value = "用户名", required = true)
+                               @RequestParam(name = "username", required = true)
+                                       String username,
+                               @ApiParam(name = "password", value = "Me密码", required = true)
+                               @RequestParam(name = "password", required = true)
+                                       String password,
+                               @ApiParam(name = "remeberMe", value = "记住我", required = false)
+                               @RequestParam(name = "remeberMe", required = false)
+                                       String remeberMe) {
+
+
+
+
+
+
+        return null;
+    }
 
 }
+
+
+
+
+
+
+
 
 
 
---
+++ my-site/src/main/java/com/huang/mysite/entity/User.java
@@ -0,0 +1,21 @@
+package com.huang.mysite.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+
+    private Integer id;
+    private String username;
+    private String password;
+
+
+
+}
+
+
+
+
+
+
